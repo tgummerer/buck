022cbafe3e16fe7b9174b0c81f88792f4bde1a71
Thomas Gummerer <t.gummerer@gmail.com>
2015-05-28 23:28:45 +0200

    Add android_instrumentation_test
    Summary:
    Add an android_instrumentation_test rule, which allows buck test to
    execute instrumentation tests directly.  Automatically executes the
    tests on all attached devices.
    
    Test plan:
    buck test
    Manually test the functionality on a simple test repository, with
    various device configurations.
    Add simple test project to test/com/facebook/buck/android/testdata.

---
+++ src/com/facebook/buck/android/ApkInstallStep.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.android;
+
+import com.facebook.buck.event.BuckEventBus;
+import com.facebook.buck.rules.InstallableApk;
+import com.facebook.buck.step.ExecutionContext;
+import com.facebook.buck.step.Step;
+import com.facebook.buck.util.Console;
+import com.google.common.base.Optional;
+
+import com.android.ddmlib.IDevice;
+
+public class ApkInstallStep implements Step {
+
+  private final InstallableApk installableApk;
+
+  public ApkInstallStep(InstallableApk apk) {
+    this.installableApk = apk;
+  }
+
+  @Override
+  public int execute(ExecutionContext context) throws InterruptedException {
+    final AdbHelper adbHelper = new AdbHelper(
+        new AdbOptions(0, true),
+        new TargetDeviceOptions(),
+        context,
+        Optional.<Console>absent(),
+        Optional.<BuckEventBus>absent(),
+        false);
+
+    if (adbHelper.getDevices().isEmpty()) {
+      return 0;
+    }
+
+    if (!adbHelper.installApk(installableApk, false)) {
+      return 1;
+    }
+    return 0;
+  }
+
+  @Override
+  public String getShortName() {
+    return "install apk";
+  }
+
+  @Override
+  public String getDescription(ExecutionContext context) {
+    final AdbHelper adbHelper = new AdbHelper(
+        new AdbOptions(0, true),
+        new TargetDeviceOptions(),
+        context,
+        Optional.<Console>absent(),
+        Optional.<BuckEventBus>absent(),
+        false);
+
+    StringBuilder builder = new StringBuilder();
+
+    try {
+      for (IDevice device : adbHelper.getDevices()) {
+        if (builder.length() != 0) {
+          builder.append("\n");
+        }
+        builder.append("adb -s ");
+        builder.append(device.getSerialNumber());
+        builder.append(" install ");
+        builder.append(installableApk.getApkPath().toString());
+      }
+    } catch (InterruptedException e) {
+    }
+    return builder.toString();
+  }
+
+}
